9cd82328e643d5436cfa82e16bbc83bf27580bc7
burak-senel <buurak.senel@gmail.com>
2024-06-09 14:53:43 +0300

    tasks done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,29 @@
 package org.example;
 
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+    public static boolean checkForPalindrome(String text){
+       String cleanText= text.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        String reversedText = new StringBuilder(cleanText).reverse().toString();
+        return cleanText.equals(reversedText);
+    }
+public static String convertDecimalToBinary(int decimal){
+    LinkedList<Integer>binary=new LinkedList<>();
+    while(decimal>0){
+        int remain=decimal%2;
+        binary.addFirst(remain);
+        decimal=decimal/2;
+    }
+    StringBuilder binaryString = new StringBuilder();
+    for (int bit : binary) {
+        binaryString.append(bit);
     }
 
+    return binaryString.toString();
+}
 }
